db1564cdc51dc4429968b9bbe3c050fbdb49aaa8
ben-schmid <ben.schmid1@gmail.com>
2024-10-16 12:20:28 -0400

    Finished server.py and added files

--- server.py
+++ server.py
@@ -6,29 +6,76 @@ import os
 # hash to save client cache
 client_cache = {}
 max_clients = 3
+current_clients = 0
 
 def handle_client(client_socket, client_name):
+    global current_clients
+    current_clients += 1
     #add start time to client cache when they join
-    client_cache[client_name] = {"start_time": datetime.now(), "end_time": None}
+    connection_start = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    #add client to hash
+    client_cache[client_name] = {"start_time": connection_start, "end_time": None}
     while True:
-        try:
-            message = client_socket.recv(1024).decode()
-            if message.lower() == "exit":
-                print(f"{client_name} has disconnected.")
+        
+        message = client_socket.recv(1024).decode()
+        if message.lower() == "exit":
+            print(f"{client_name} has disconnected.")
 
-                #add endtime to client cache when they disconnect
-                client_cache[client_name]['end_time'] = datetime.now()
-                client_socket.close()
-                break
-           # elif message.lower() == "status":
-                #logic for status
-           # elif message.low() == "list":
-                #logic for list directory
-            else:
-                reponse = message + " ACK"
-                client_socket.send(reponse.encode())
-        except:
+            #add endtime to client cache when they disconnect
+            client_cache[client_name]['end_time'] = datetime.now()
+            client_socket.close()
+            current_clients -= 1
             break
+        elif message.lower() == "status":
+            status_message = ""
+            
+            #loop to list the cache of all clients that have connected
+            for client, times in client_cache.items():
+
+                #extract times from hash
+                start_time = times["start_time"]
+                if times["end_time"] != None:
+                    end_time = times["end_time"]
+                else:
+                    end_time = "Still Connected"
+                status_message += f"{client} - Start: {start_time}, End: {end_time}\n"
+            
+            #If loop is not run, there are no clients that have connected
+            if status_message == "":
+                status_message = "No clients connected"
+            
+            #send status to clients
+            client_socket.send(status_message.encode())
+        elif message.lower() == "list":
+            files = os.listdir("/Users/benschmid/vscode/CP372/files")
+            file_list = "\n".join(files)
+
+            if not files:
+                file_list = "No files in the directory"
+            
+            client_socket.send(file_list.encode())
+
+        elif message.lower().startswith("get "):
+            filename = message[4:]
+            filepath = os.path.join("/Users/benschmid/vscode/CP372/files", filename)
+
+            if os.path.exists(filepath):
+                with open(filepath, "rb") as file:
+                    contents = file.read(1024)
+                    while contents:
+                        client_socket.send(contents)
+                        contents = file.read(1024)
+                
+                client_socket.send("EOF".encode())
+            else:
+                client_socket.send("File not found".encode)
+        
+
+            
+        else:
+            reponse = message + " ACK"
+            client_socket.send(reponse.encode())
+        
 
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,7 +85,7 @@ def server():
 
     client_id = 1
     while True:
-        if len(client_cache) < max_clients:
+        if current_clients <=3:
             client_socket, addr = server_socket.accept()
             client_name = f"Client  {client_id}"
             print(f"Connection from {addr} as {client_name}")
